c5881ddd33e0e8e65c03abe1b6b41e663b8dc2c6
name <mail@mail.com>
2024-01-17 11:37:58 +0100

    Ajout du package vue

---
+++ ludotheque/src/main/java/vue/MainVue.java
@@ -0,0 +1,12 @@
+package vue;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainVue extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	}
+
+}
