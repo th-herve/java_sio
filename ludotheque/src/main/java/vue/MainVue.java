19cea583d43605aa3dd6cb8a567b7abc9203da29
H-deve <hajjo.dev@gmail.com>
2024-04-09 15:41:18 +0200

    moatasm

--- ludotheque/src/main/java/vue/MainVue.java
+++ ludotheque/src/main/java/vue/MainVue.java
@@ -10,7 +10,20 @@ public class MainVue extends Application {
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
-      
+        // Create a root node for the scene
+        StackPane root = new StackPane();
+
+        // Create the scene
+        Scene scene = new Scene(root, 400, 300);
+
+        // Set the scene to the stage
+        primaryStage.setScene(scene);
+
+        // Set the title of the stage
+        primaryStage.setTitle("JavaFX Test Application");
+
+        // Show the stage
+        primaryStage.show();
     }
 
 
