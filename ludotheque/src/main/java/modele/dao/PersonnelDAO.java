52064410fc1a14cb6d688e87c51e01149702c52c
Thibault <thibault.herve1@gmail.com>
2024-04-09 20:25:30 +0200

    Update personnel pour extends personne

--- ludotheque/src/main/java/modele/dao/PersonnelDAO.java
+++ ludotheque/src/main/java/modele/dao/PersonnelDAO.java
@@ -1,20 +1,23 @@
 package modele.dao;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Date;
-import java.time.LocalDate;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import modele.Personne;
 import modele.Personnel;
 
 
 public class PersonnelDAO extends DAO<Personnel> {
 
+	private static PersonneDAO personneDao;
+
 	private static final String TABLE = "Personnel";
-	private static final String CLE_PRIMAIRE = "id_Personne";
+	private static final String CLE_PRIMAIRE = "idPersonne";
 
 	private static final String ROLE = "role";
 	private static final String DATE_ENTREE = "dateEntree";
@@ -31,19 +34,29 @@ public class PersonnelDAO extends DAO<Personnel> {
 
 	private PersonnelDAO() {
 		super();
+		personneDao = PersonneDAO.getIntstance();
 	}
 
 	@Override
 	public boolean create(Personnel personnel) {
 		boolean success = true;
 		try {
-			String requete = "INSERT INTO " + TABLE + " (" + ROLE + ", " + DATE_ENTREE + ", " + DATE_SORTIE
-					+ ") VALUES (?, ?, ?)";
+
+			int idPers = personnel.getId();
+			
+			// si l'id est 0, il faut également créer une nouvelle personne (si != 0 ca veut dire que la personne assciée à l'adherent existe déjà)
+			if (idPers == 0) {
+				personneDao.create((Personne)personnel);
+			}
+
+			String requete = "INSERT INTO " + TABLE + " (" + CLE_PRIMAIRE + ", " + ROLE + ", " + DATE_ENTREE + ", " + DATE_SORTIE
+					+ ") VALUES (?, ?, ?, ?)";
 			PreparedStatement pst = Connexion.getInstance().prepareStatement(requete, Statement.RETURN_GENERATED_KEYS);
 
-			pst.setString(1, personnel.getRole());
-			pst.setObject(2, personnel.getDateEntree());
-			pst.setObject(3, personnel.getDateSortie());
+			pst.setInt(1, personnel.getId());
+			pst.setString(2, personnel.getRole());
+			pst.setObject(3, personnel.getDateEntree());
+			pst.setObject(4, personnel.getDateSortie());
 //            Date dateEntree = Date.valueOf(personnel.getDateEntree()).toLocalDate());
 //            pst.setDate(2, dateEntree);
 //            Date dateSortie = Date.valueOf(personnel.getDateSortie().toLocalDate());
@@ -53,10 +66,10 @@ public class PersonnelDAO extends DAO<Personnel> {
 
 			ResultSet rs = pst.getGeneratedKeys();
 			if (rs.next()) {
-				personnel.setId_Personne(rs.getInt(1));
+				personnel.setId(rs.getInt(1));
 			}
 
-			donnees.put(personnel.getId_Personne(), personnel);
+			donnees.put(personnel.getId(), personnel);
 		} catch (SQLException e) {
 			success = false;
 			e.printStackTrace();
@@ -68,12 +81,19 @@ public class PersonnelDAO extends DAO<Personnel> {
 	public boolean delete(Personnel personnel) {
 		boolean success = true;
 		try {
-			int id = personnel.getId_Personne();
+			Personne personne = (Personne)personnel;
+			int id = personnel.getId();
+
 			String query = "DELETE FROM " + TABLE + " WHERE " + CLE_PRIMAIRE + " = ?";
 			PreparedStatement pst = Connexion.getInstance().prepareStatement(query);
 			pst.setInt(1, id);
 			pst.executeUpdate();
+
 			donnees.remove(id);
+
+			// on suprimme également la personne associée
+			personneDao.delete(personne);
+
 		} catch (SQLException e) {
 			success = false;
 			e.printStackTrace();
@@ -94,11 +114,16 @@ public class PersonnelDAO extends DAO<Personnel> {
 			pst.setDate(2, dateEntree);
 			Date dateSortie = Date.valueOf(personnel.getDateSortie().toLocalDate());
 			pst.setDate(3, dateSortie);
-			pst.setInt(4, personnel.getId_Personne());
+			pst.setInt(4, personnel.getId());
 
 			pst.executeUpdate();
 
-			donnees.put(personnel.getId_Personne(), personnel);
+			donnees.put(personnel.getId(), personnel);
+
+			// update la personne associée
+			personneDao.update((Personne) personnel);
+
+
 		} catch (SQLException e) {
 			success = false;
 			e.printStackTrace();
@@ -107,22 +132,35 @@ public class PersonnelDAO extends DAO<Personnel> {
 	}
 
 	@Override
-	public Personnel read(int id_Personne) {
+	public Personnel read(int idPersonne) {
+
 		Personnel personnel = null;
-		if (donnees.containsKey(id_Personne)) {
-			personnel = donnees.get(id_Personne);
+		Personne personne = null;
+
+		if (donnees.containsKey(idPersonne)) {
+			personnel = donnees.get(idPersonne);
 		} else {
 			try {
-				String query = "SELECT * FROM " + TABLE + " WHERE " + CLE_PRIMAIRE + " = " + id_Personne;
+				String query = "SELECT * FROM " + TABLE + " WHERE " + CLE_PRIMAIRE + " = " + idPersonne;
 				ResultSet rs = Connexion.executeQuery(query);
 				rs.next();
 
 				String role = rs.getString(ROLE);
 				LocalDateTime dateEntree = rs.getTimestamp(DATE_ENTREE).toLocalDateTime();
-				LocalDateTime dateSortie = rs.getTimestamp(DATE_SORTIE).toLocalDateTime();
 
-				personnel = new Personnel(id_Personne, role, dateEntree, dateSortie);
-				donnees.put(id_Personne, personnel);
+				// pour la date de sortie, comme elle peut etre null dans la bd, il faut controler
+				Timestamp timestamp = rs.getTimestamp(DATE_SORTIE);
+				LocalDateTime dateSortie = null;
+				if (timestamp != null) {
+					dateSortie = timestamp.toLocalDateTime();
+				}
+
+				personne = personneDao.read(idPersonne);
+				personnel = new Personnel(personne.getNom(), personne.getPrenom(), personne.getEmail(), personne.getAdresse(), personne.getTel(), role, dateEntree, dateSortie);
+				personnel.setId(personne.getId());
+
+
+				donnees.put(idPersonne, personnel);
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
