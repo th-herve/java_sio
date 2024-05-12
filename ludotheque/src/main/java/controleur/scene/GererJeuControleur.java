b869c1849bea768476193a8139c693c040c10670
thibault <thibault.herve1@gmail.com>
2024-05-12 21:45:26 +0200

    Ajoute bouton pour ouvrir gestion des jeux physique

--- ludotheque/src/main/java/controleur/scene/GererJeuControleur.java
+++ ludotheque/src/main/java/controleur/scene/GererJeuControleur.java
@@ -209,6 +209,11 @@ public class GererJeuControleur extends SceneControleur{
 	}
 	
 
+	public void openGererJeuPhysique() {
+		Jeu jeu = tableJeu.getSelectionModel().getSelectedItem();
+		this.switchToGererJeuPhysique(jeu.getId());
+	}
+
 	//	private void handleActionFloat(CellEditEvent<Jeu, String> event) {
 	//		TableColumn<Jeu, String> column = event.getTableColumn();
 	//		Jeu jeu = event.getRowValue();
