e22165bc7ff145177f5f3f53d35b137b08f2a8a5
Jeanne <jeanne.jestin2@gmail.com>
2024-05-29 13:49:19 +0200

    'test'

--- ludotheque/src/main/java/modele/dao/Connexion.java
+++ ludotheque/src/main/java/modele/dao/Connexion.java
@@ -23,8 +23,8 @@ public class Connexion {
 
 
 //	private static final String SQL_SERVER = "DESKTOP-7O2DRQE\\SQLEXPRESS04"; // Moatasm
-//	private static final String SQL_SERVER = "DESKTOP-7O2DRQE\\SQLEXPRESS03"; // Jeanne
-	private static final String SQL_SERVER = "DESKTOP-7O2DRQE\\SQLEXPRESS"; // Thibault
+	private static final String SQL_SERVER = "DESKTOP-7O2DRQE\\SQLEXPRESS03"; // Jeanne
+//	private static final String SQL_SERVER = "DESKTOP-7O2DRQE\\SQLEXPRESS"; // Thibault
 
 
 	private static final String BASE_DE_DONNEES = "bd_Ludotheque";
